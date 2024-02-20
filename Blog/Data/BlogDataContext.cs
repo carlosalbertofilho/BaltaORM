3d777e3959981ae6c1bf63ce9b3a0a92ba668804
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-02-20 13:40:06 -0300

    #DbContext:
    Updated project references, renamed properties, added DbContext
    
    Updated the `Blog.csproj` file to include references to the `Microsoft.EntityFrameworkCore` and `Microsoft.EntityFrameworkCore.SqlServer` packages. Renamed `CreatedAt` and `LastUpdate` properties in `Post.cs` to `CreateDate` and `LastUpdateDate` respectively. Created a new `BlogDataContext.cs` file that defines a new `DbContext` named `BlogDataContext` with `DbSet` properties for various entities and configured to use SQL Server.

---
+++ Blog/Data/BlogDataContext.cs
@@ -0,0 +1,28 @@
+﻿
+
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data
+{
+    public class BlogDataContext : DbContext
+    {
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<PostTag> PostTags { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<UserRole> UserRoles { get; set; }
+
+        /*
+        * Docker Test C = 10.211.55.2
+        * "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;";
+        * Docker Test H 
+        * """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
+        */
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer(
+                """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""");
+    }
+}
--- Blog/Models/Post.cs
+++ Blog/Models/Post.cs
@@ -10,7 +10,7 @@ namespace Blog.Models
         public string? Summary { get; set; }
         public string? Body { get; set; }
         public string? Slug { get; set; }
-        public DateTime CreatedAt { get; set; }
-        public DateTime LastUpdate { get; set; }
+        public DateTime CreateDate { get; set; }
+        public DateTime LastUpdateDate { get; set; }
     }
 }
