5d716e46ae28745a343b03a5e2daf075324f039b
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-02-20 15:13:01 -0300

    #DB:
    Update Entity Framework mappings and add basic DB operations
    
    Updated Entity Framework mappings in `BlogDataContext.cs`, `Category.cs`, `Post.cs`, `Role.cs`, `Tag.cs`, and `User.cs`. Commented out `PostTags` and `UserRoles` DbSet properties in `BlogDataContext.cs`, preventing direct queries or updates to these tables. Added `System.ComponentModel.DataAnnotations` and `System.ComponentModel.DataAnnotations.Schema` namespaces for data validation and database schema settings. Added `Table` attribute to class declarations and `Key` attribute to `Id` properties. Changed `Id`, `Name`, and `Slug` properties in `Role.cs` from auto-implemented to explicit `get` and `set` accessors. Replaced "Hello, World!" console output in `Program.cs` with code to create a new `BlogDataContext` instance, add a new `Tag`, and save changes to the database.

--- Blog/Models/User.cs
+++ Blog/Models/User.cs
@@ -1,8 +1,13 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Blog.Models
 {
+    [Table("User")]
     public class User
     {
+        [Key]
         public int Id { get; set; }
         public string? Name { get; set; }
         public string? Email { get; set; }
