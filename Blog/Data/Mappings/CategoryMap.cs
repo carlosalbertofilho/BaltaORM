2feee9b2fa9042a11b8265b50df62f29fbef8f17
carlosalbertofilho <solrac.cassf@hotmail.com>
2024-02-29 16:14:08 -0300

    #Mapping:
    Created CategoryMap class for Category entity configuration
    
    Created a new class `CategoryMap` in the `Blog.Data.Mappings` namespace to configure the `Category` entity in the Entity Framework Core. The `Configure` method sets up the properties and relationships of the `Category` entity. The `Category` table in the database is specified, and the primary key is mapped to the `Id` property. The `Id` property is set to auto-increment. The `Name` and `Slug` properties are mapped to columns in the database, set as required, their type is set to `NVARCHAR`, and their length is limited to 80 characters. A unique index is created for the `Slug` property.

---
+++ Blog/Data/Mappings/CategoryMap.cs
@@ -0,0 +1,47 @@
+﻿
+
+using Blog.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blog.Data.Mappings
+{
+    public class CategoryMap : IEntityTypeConfiguration<Category>
+    {
+        public void Configure(EntityTypeBuilder<Category> builder)
+        {
+            // Especifica o nome da Tabela
+            // equivale ao [Table("Category")] do EF6
+            builder.ToTable("Category");
+
+            // Mapeia a chave primária
+            // equivale ao [Key] do EF6
+            builder.HasKey(category => category.Id);
+
+            // Mapeia o campo Id como autoincremento
+            // equivale ao [DatabaseGenerated(DatabaseGeneratedOption.Identity)] do EF6
+            builder.Property(category => category.Id)
+                .ValueGeneratedOnAdd()
+                .UseIdentityColumn(); // IDENTITY(1,1)
+
+            // Mapeia o campo Name como nvarchar(80)
+            // equivale ao [StringLength(80)] do EF6
+            builder.Property(category => category.Name)
+                .IsRequired()
+                .HasColumnName("Name")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            // Mapeia o campo Slug como nvarchar(80)
+            builder.Property(category => category.Slug)
+                .IsRequired()
+                .HasColumnName("Slug")
+                .HasColumnType("NVARCHAR")
+                .HasMaxLength(80);
+
+            // Mapeia o índice único para o campo Slug
+            builder.HasIndex(category => category.Slug, "IX_Category_Slug")
+                .IsUnique();
+        }
+    }
+}
