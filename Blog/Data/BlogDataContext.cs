aba0af16a0f2c9b83062ca6a26332509e0bdc41d
carlosalbertofilho <carlosalberto_filho@outlook.com>
2024-03-01 09:33:46 -0300

    #DB:
    Update import and SQL Server connection in BlogDataContext.cs

--- Blog/Data/BlogDataContext.cs
+++ Blog/Data/BlogDataContext.cs
@@ -1,7 +1,4 @@
-﻿
-
-using Blog.Models;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
@@ -13,10 +10,10 @@ namespace Blog.Data
         * Docker Test C = 10.211.55.2
         * "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;"
         * Docker Test H 
-        * """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""";
+        * """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;"""
         */
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
             => optionsBuilder.UseSqlServer(
-                "Server=10.211.55.2,1433;Database=Blog;User Id=sa;Password=1q2w3e4r@#$;Trusted_Connection=False;TrustServerCertificate=True;");
+                """Data Source=DESKTOP-4P9S4FF\SQLEXPRESS;Initial Catalog=Blog;Integrated Security=True; TrustServerCertificate=True;""");
     }
 }
